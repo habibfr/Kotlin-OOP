ee360df7469dd8147ba16a470785256e5812e6fd
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-05-10 22:18:02 +0700

    function overloading

--- 03 - Function/Person.kt
+++ 03 - Function/Person.kt
@@ -7,6 +7,10 @@ class People{
     println("hello $name")
   }
 
+  fun sayHello(fName: String, lName: String){
+    println("hello $fName $lName")
+  }
+
   fun run(){
     println("$firstName run")
   }
@@ -22,6 +26,7 @@ fun main(){
   people1.middleName = "fatkhul"
   people1.lastName = "rohman"
   people1.sayHello("budi")
+  people1.sayHello("ceri", "cans")
   people1.run()
   println(people1.getFullName())
 }
