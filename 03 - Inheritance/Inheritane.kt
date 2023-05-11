d17a9d231a4533740d2ce6eb3b4bc3822649a300
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-05-11 15:27:29 +0700

    function overriding

--- 03 - Inheritance/Inheritane.kt
+++ 03 - Inheritance/Inheritane.kt
@@ -1,35 +1,62 @@
 // open to change this class become parents
 open class Emoployee(var name: String){
-  fun sayHello(name: String){
+  open fun sayHello(name: String){
     println("hello $name, my name is ${this.name}")
   }
 }
 
 open class Manager(name: String) : Emoployee(name){
-  fun sayManager(){
-    println("hello saya manager ${this.name}")
+  // fun sayManager(){
+  //   println("hello saya manager ${this.name}")
+  // }
+
+  // final cant override by child
+  final override fun sayHello(name: String){
+    println("hello $name, my name is Manager ${this.name}")
   }
 }
 
-class VicePresident(name: String) : Manager(name){
-  fun sayVP(){
-    println("Vice President disini")
+class VicePresident(name: String) : Emoployee(name){
+  // fun sayVP(){
+  //   println("Vice President disini")
+  // }
+
+  override fun sayHello(name: String){
+    println("hello $name, my name is Vice President ${this.name}")
   }
+}
+
+class SuperManager(name: String) : Manager(name){
+  // cant ovveride fun parents cause its fun is final
+  // override fun sayHello(name: String){
+  //   println("hello $name, my name is Vice President ${this.name}")
+  // }
 }
 
 fun main(){
-  val manager1 = Manager("Habib");
+  // inheritance
+  // val manager1 = Manager("Habib");
 
-  val emp1 = Emoployee("Budi")
+  // val emp1 = Emoployee("Budi")
 
-  val vp1 = VicePresident("Api")
+  // val vp1 = VicePresident("Api")
 
-  manager1.sayManager()
-  manager1.sayHello("Api")
+  // manager1.sayManager()
+  // manager1.sayHello("Api")
 
-  emp1.sayHello("Api")
+  // emp1.sayHello("Api")
 
-  vp1.sayHello("Habib")
-  vp1.sayManager()
-  vp1.sayVP()
+  // vp1.sayHello("Habib")
+  // vp1.sayManager()
+  // vp1.sayVP()
+
+  // properties overriding
+  val manager = Manager("Habib")
+  val emp = Emoployee("Budi")
+  val vp = VicePresident("Api")
+
+  manager.sayHello("Budi")
+  emp.sayHello("Api")
+  vp.sayHello("Habib")
+
 }
