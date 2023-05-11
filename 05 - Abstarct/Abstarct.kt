35e432773a48d2458c120a23f1162f9710494e51
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-05-11 20:14:25 +0700

    abstract properties & function

--- 05 - Abstarct/Abstarct.kt
+++ 05 - Abstarct/Abstarct.kt
@@ -1,9 +1,29 @@
+// abstract class
 abstract class Location(val name: String)
 
 class City(name: String) : Location(name)
 
 class Country(name: String) : Location(name)
 
+// abstract properties dan function
+abstract class Animal{
+  abstract val name: String
+  abstract fun run()
+}
+
+class Cat : Animal(){
+  override val name: String = "Cat"
+  override fun run(){
+    println("${this.name} sedang berlari")
+  }
+}
+
 fun main() {
-  val city = City("city")
+  // abstract class
+  // val city = City("city")
+
+  // abstract properties & function
+  val cat = Cat()
+  cat.run()
+
 }
